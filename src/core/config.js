d619f0b5e4eaadeaeaaca81547499118cd236b84
LandRover <github.com@landroverz.com>
2016-05-27 08:49:31 +0000

    removed debug stuff

--- src/core/config.js
+++ src/core/config.js
@@ -9,36 +9,34 @@ import _ from '../utils/__';
  */
 class Config extends BaseSingleton {
     static CLASS = 'Config';
-    
+
     CONFIG_DEFAULTS = {
         el: '#social-music-player',
-        
+
         loop: false,
         volume: 100,
         fadeoutTime: 2, // seconds
-        
+
         fullscreen: true,
-        
+
         width: '640px',
         height: '480px',
-        
+
         logLevel: 'DEBUG'
     };
 
 
     /**
-     * 
+     *
      */
     init(settings) {
-        _.merge(this, this.CONFIG_DEFAULTS, settings, {width: 1});
+        _.merge(this, this.CONFIG_DEFAULTS, settings);
         this.logger.debug('Config create fired');
-        
-        window.og = this;
     }
 
-    
-    
-    
+
+
+
 }
 
 export default Config;
