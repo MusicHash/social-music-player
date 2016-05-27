c365b683ba01504f5046dd10ecd150b8c7240985
LandRover <github.com@landroverz.com>
2016-05-27 09:24:35 +0000

    config overrides set

--- src/core/config.js
+++ src/core/config.js
@@ -4,39 +4,44 @@ import BaseSingleton from '../base/base_singleton';
 import {SYSTEM_EVENTS} from '../events/events';
 import _ from '../utils/__';
 
+let CONFIG_DEFAULTS = {
+    el: '#social-music-player',
+
+    loop: false,
+    volume: 100,
+    fadeoutTime: 2, // seconds
+
+    fullscreen: true,
+
+    width: '640px',
+    height: '480px',
+
+    logLevel: 'DEBUG'
+};
+
 /**
  *
  */
 class Config extends BaseSingleton {
     static CLASS = 'Config';
 
-    CONFIG_DEFAULTS = {
-        el: '#social-music-player',
-
-        loop: false,
-        volume: 100,
-        fadeoutTime: 2, // seconds
-
-        fullscreen: true,
-
-        width: '640px',
-        height: '480px',
-
-        logLevel: 'DEBUG'
-    };
-
+    _defaultSet = false;
 
     /**
      *
      */
     init(settings) {
-        _.merge(this, this.CONFIG_DEFAULTS, settings);
         this.logger.debug('Config create fired');
+
+        // init defaults only once.
+        if (!this._defaultSet) {
+            _.merge(this, CONFIG_DEFAULTS);
+            this._defaultSet = true;
+        }
+
+        // modify config data
+        _.merge(this, settings);
     }
-
-
-
-
 }
 
 export default Config;
