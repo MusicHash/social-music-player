0607a86843ac5961b5fa3bb6d94c4492075edd77
LandRover <github.com@landroverz.com>
2016-05-29 19:20:35 +0000

    providers stuff

---
+++ src/components/player/providers/base_provider.js
@@ -0,0 +1,11 @@
+import BaseObject from '../../../base/base_object';
+import _ from '../utils/__';
+
+/**
+ *
+ */
+class BaseProvider extends BaseObject {
+    static CLASS = 'BaseProvider';
+}
+
+export default BaseProvider;
---
+++ src/components/player/providers/youtube/youtube_provider.js
@@ -0,0 +1,11 @@
+import BaseProvider from '../base/base_provider';
+import _ from '../utils/__';
+
+/**
+ *
+ */
+class YoutubeProvider extends BaseProvider {
+    static CLASS = 'YoutubeProvider';
+}
+
+export default YoutubeProvider;
