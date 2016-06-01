128f3c533af3b755fc204146a936df8957cf31c8
LandRover <github.com@landroverz.com>
2016-06-01 06:23:51 +0000

    provider controller fixed ref

--- src/components/player/player_controller.js
+++ src/components/player/player_controller.js
@@ -4,13 +4,9 @@ import DOM from '../../utils/dom';
 import {PROVIDERS_LIST} from '../../constants/providers';
 
 import YoutubeProvider from './providers/youtube/youtube_provider';
-import YoutubeModel from './providers/youtube/youtube_model';
-
 import VimeoProvider from './providers/vimeo/vimeo_provider';
-import VimeoModel from './providers/vimeo/vimeo_model';
-
 import SoundCloudProvider from './providers/soundcloud/soundcloud_provider';
-import SoundCloudModel from './providers/soundcloud/soundcloud_model';
+
 
 
 /**
@@ -21,6 +17,7 @@ class PlayerController extends BaseController {
     static selectorClass = '.smp_player';
 
     songModel = null;
+    Provider = null;
 
 
     /**
@@ -60,23 +57,24 @@ class PlayerController extends BaseController {
     getProvider() {
         if (null !== this.Provider) return this.Provider;
 
-        switch (this.songModel.CLASS) {
-            case YoutubeProvider.CLASS:
+        switch (this.songModel.provider) {
+            case PROVIDERS_LIST.YOUTUBE:
                 this.Provider = YoutubeProvider.create(this.songModel);
                 break;
 
-            case VimeoProvider.CLASS:
+            case PROVIDERS_LIST.VIMEO:
                 this.Provider = VimeoProvider.create(this.songModel);
                 break;
 
-            case SoundCloudProvider.CLASS:
+            case PROVIDERS_LIST.SOUNDCLOUD:
                 this.Provider = SoundCloudProvider.create(this.songModel);
                 break;
 
             default:
                 throw new Error('Provider was not found');
+                break;
 
-            return null;
+            return this.Provider;
         }
     }
 }
