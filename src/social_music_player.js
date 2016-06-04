cc1892b8f1324fce9840acd85a8837402e1a78c5
LandRover <github.com@landroverz.com>
2016-06-04 06:15:04 +0000

    scss

--- src/social_music_player.js
+++ src/social_music_player.js
@@ -1,5 +1,7 @@
 import API from './core/api';
 
+require('./smp.scss');
+
 /**
  * Setups SocialPlayer and binds Core to the actual DOM window.
  * NOTE: This code is self-executing. This is necessary in order to correctly
@@ -9,7 +11,7 @@ window.SocialMusicPlayer = window.SocialMusicPlayer || new API();
 
 /**
  * Load SMP ready method after async loading is completed.
- * 
+ *
  * Usually will call up initialize which can begin working with the DOM.
  */
 window.SMPAsyncInit();
